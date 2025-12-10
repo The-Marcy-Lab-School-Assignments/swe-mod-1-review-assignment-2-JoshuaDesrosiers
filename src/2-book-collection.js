fefd5d9043106a908af90280ca4c1912883f334b
JoshuaDesrosiers <desrosiersjoshua3@gmail.com>
2025-12-10 10:51:06 -0500

    almost done

--- src/2-book-collection.js
+++ src/2-book-collection.js
@@ -11,5 +11,12 @@ const bookInventory = {
 // "Beloved: 5 copies available"
 // etc...
 
+for(let x of Object.keys(bookInventory))
+    console.log(`${x}: ${bookInventory[x]} copies available`)
+
 // Part B: Calculate and log the total number of books across all titles (should be 15)
 // Hint: You'll need a variable to keep track of the running total as you loop through the keys
+let _bin = 0
+for(let x of Object.keys(bookInventory))
+    _bin+=bookInventory[x]
+console.log(_bin)
--- src/3-reference-types.js
+++ src/3-reference-types.js
@@ -4,15 +4,21 @@ const team1 = {
   losses: 20
 };
 
-const team2 = team1;
-team2.wins = 50;
-team2.losses = 15;
+// const team2 = team1;
+// team2.wins = 50;
+// team2.losses = 15;
 
 /* 
 Part A: What will team1.wins and team1.losses be after this code runs? Write your prediction as a comment below:
 
 - Prediction:
 
+same thing its using reference not a copy
+
 Part B: Fix the code so that team2 is a TRUE COPY of team1. After making your fix, changing team2 should NOT affect team1.
 Part C: Verify your fix by logging both team1.wins (should still be 45) and team2.wins (should be 50)
 */
+
+const team2 = {...team1};
+team2.wins = 50;
+team2.losses = 15;
